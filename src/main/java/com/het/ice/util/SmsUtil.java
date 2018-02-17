f60a1a3471439d04a631ff9bd2ae8ec6a414ced1
houenteng <314241517@qq.com>
2018-02-17 22:06:52 +0800

    短信服务恢复

--- src/main/java/com/het/ice/util/SmsUtil.java
+++ src/main/java/com/het/ice/util/SmsUtil.java
@@ -1,17 +1,20 @@
 package com.het.ice.util;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import com.het.ice.enums.DingTypeEnum;
+import net.sf.json.JSONArray;
+
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import com.aliyuncs.DefaultAcsClient;
-import com.aliyuncs.IAcsClient;
-import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.exceptions.ServerException;
-import com.aliyuncs.profile.DefaultProfile;
-import com.aliyuncs.profile.IClientProfile;
-import com.aliyuncs.sms.model.v20160927.SingleSendSmsRequest;
-import com.aliyuncs.sms.model.v20160927.SingleSendSmsResponse;
 
 /**
  * 短信工具
@@ -20,21 +23,41 @@ import com.aliyuncs.sms.model.v20160927.SingleSendSmsResponse;
  *
  */
 public class SmsUtil {
-
 	public static boolean send(String phone, String code) {
 		try {
-			IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAIifrLWuAMwVpR",
-					"DdsIl69gVLedduE1hEJUkWb6Sy3EAF");
-			DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", "Sms", "sms.aliyuncs.com");
+			final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
+			final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
+			final String accessKeyId = "LTAIifrLWuAMwVpR";//你的accessKeyId,参考本文档步骤2
+			final String accessKeySecret = "DdsIl69gVLedduE1hEJUkWb6Sy3EAF";//你的accessKeySecret，参考本文档步骤2
 
-			IAcsClient client = new DefaultAcsClient(profile);
-			SingleSendSmsRequest request = new SingleSendSmsRequest();
+			IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
+					accessKeySecret);
+			DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+
+			IAcsClient acsClient = new DefaultAcsClient(profile);
+
+			SendSmsRequest request = new SendSmsRequest();
+			//使用post提交
+			request.setMethod(MethodType.POST);
+			//必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
+			request.setPhoneNumbers(phone);
+			//必填:短信签名-可在短信控制台中找到
 			request.setSignName("白云冷饮");
+			//必填:短信模板-可在短信控制台中找到
 			request.setTemplateCode("SMS_33555146");
-			request.setParamString("{\"code\": \"" + code + "\"}");
-			request.setRecNum(phone);
-			SingleSendSmsResponse httpResponse = client.getAcsResponse(request);
-			return true;
+			//可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+			//友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
+			request.setTemplateParam("{\"code\": \"" + code + "\"}");
+
+			SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
+			if(sendSmsResponse.getCode() != null && sendSmsResponse.getCode().equals("OK")) {
+				return true;
+			}
+			JSONArray mobls = new JSONArray();
+			mobls.add("18757169657");
+
+			DingdingSendUtil.send(DingTypeEnum.TEXT, "短信服务异常", sendSmsResponse.getCode(), mobls, false);
+			return false;
 		} catch (ServerException e) {
 			e.printStackTrace();
 			return false;
--- src/main/java/com/het/ice/util/Test.java
+++ src/main/java/com/het/ice/util/Test.java
@@ -3,9 +3,11 @@ package com.het.ice.util;
 public class Test {
 
     public static void main(String args[]) {
-        Test t1 = new Test();
+        /*Test t1 = new Test();
         Test t2 = new Test();
 
-        System.out.println(t1.equals(t2));
+        System.out.println(t1.equals(t2));*/
+
+        SmsUtil.send("15888861560", "123456");
     }
 }

c9e653a0a7eb545c3227a9c30aee090c509f0cbe
houenteng <314241517@qq.com>
2018-02-18 17:21:37 +0800

    update version to 20180219

--- src/main/java/com/het/ice/util/CommonConstants.java
+++ src/main/java/com/het/ice/util/CommonConstants.java
@@ -12,7 +12,7 @@ public class CommonConstants {
 
     public final static String USER_PROPERTY_LAST_STATE = "lastState";
 
-    public final static int APP_VERSION = 20180218;
+    public final static int APP_VERSION = 20180219;
 
     /** 默认业务ID */
     public final static long DEFAULT_BIZ_ID = 1L;
